fd45e96e5516a0b368f171f285156430a0e2192f
Alejo-Nino95 <alejo_120295_hotmail.com>
2022-02-19 02:58:25 -0500

    creacion bd

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -3,10 +3,37 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('dog', {
-    name: {
-      type: DataTypes.STRING,
+  sequelize.define('Raza', {
+    ID: {
+      type: DataTypes.INTEGER,
       allowNull: false,
+      unique: true,
+      primaryKey: true
     },
+    nombre: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    altura: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    peso: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    anosvida: {
+      type: DataTypes.STRING,
+    }
+  });
+  sequelize.define('Temperamento', {
+    ID: {
+      type: DataTypes.INTEGER,
+      unique: true,
+      primaryKey: true
+    },
+    nombre: {
+      type: DataTypes.STRING,
+    }
   });
 };

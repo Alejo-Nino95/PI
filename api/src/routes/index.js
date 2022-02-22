dd5e1a1a34eea94dd3d3e8fae16cd6524bf9e6bc
Alejo-Nino95 <alejo_120295_hotmail.com>
2022-02-22 16:04:13 -0500

    avance 4

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -40,6 +40,41 @@ router.get('/dogs', async (req, res) => {
     res.json(raza);
 })
 
+router.get('/dogs/:idRaza', async (req, res) => {
+    var raza;
+    const { idRaza } = req.params;
+    if (idRaza >= 1000) {
+        raza = await Raza.findOne({
+            where: { id: idRaza }
+        })
+        if (raza) return res.json(raza)
+    } else {
+        const apiData = await axios.get('https://api.thedogapi.com/v1/breeds');
+        const razaArray = apiData.data.map(r => {
+            return {
+                id: r.id,
+                nombre: r.name
+            }
+        });
+        raza = razaArray.find(r => r.id === parseInt(idRaza))
+        if (raza) {
+            const apiRaza = await axios.get(`https://api.thedogapi.com/v1/breeds/search?q=${raza.nombre}`)
+            const razaBuscada = apiRaza.data.map(r => {
+                return {
+                    imagen: `https://cdn2.thedogapi.com/images/${r.reference_image_id}.jpg`,
+                    nombre: r.name,
+                    temperamento: r.temperament,
+                    altura: r.height.metric,
+                    peso: r.weight.metric,
+                    anosvida: r.life_span
+                }
+            })
+            return res.json(razaBuscada)
+        }
+    }
+    res.status(404).send('No existe esa raza');
+})
+
 router.get('/temperament', async (req, res) => {
     var tempf = [];
     const apiData = await axios.get('https://api.thedogapi.com/v1/breeds');
@@ -63,7 +98,7 @@ router.get('/temperament', async (req, res) => {
     res.json(tempBd);
 })
 
-router.post('/dog', async (req,res) => {
+router.post('/dog', async (req, res) => {
     const { nombre, altura, peso, anosvida } = req.body;
     await Raza.create({
         id: idRaza,

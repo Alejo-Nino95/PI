b3967210e16566d6adfb68a25c9f4a022eadda4c
Alejo-Nino95 <alejo_120295_hotmail.com>
2022-03-01 01:30:38 -0500

    avance 15

--- api/tests/models/dog.spec.js
+++ api/tests/models/dog.spec.js
@@ -1,21 +1,71 @@
-const { Dog, conn } = require('../../src/db.js');
+const { Raza, conn } = require('../../src/db.js');
 const { expect } = require('chai');
 
-describe('Dog model', () => {
+describe('Raza model', () => {
   before(() => conn.authenticate()
     .catch((err) => {
       console.error('Unable to connect to the database:', err);
     }));
   describe('Validators', () => {
-    beforeEach(() => Dog.sync({ force: true }));
-    describe('name', () => {
-      it('should throw an error if name is null', (done) => {
-        Dog.create({})
-          .then(() => done(new Error('It requires a valid name')))
+    beforeEach(() => Raza.sync({ force: true }));
+    describe('id', () => {
+      it('no crea el elemento si id está nulo', (done) => {
+        Raza.create({
+          imagen: 'https://empresas.blogthinkbig.com/wp-content/uploads/2019/11/Imagen3-245003649.jpg?w=800',
+          altura: '12 - 16',
+          peso: '13 - 15',
+          anosvida: 12,
+          temperamentos: ['Aloof', 'Clownish', 'Dignified', 'Independent', 'Happy']})
+          .then(() => done('no crea el registro'))
           .catch(() => done());
       });
-      it('should work when its a valid name', () => {
-        Dog.create({ name: 'Pug' });
+    });
+    describe('nombre', () => {
+      it('no crea el elemento si nombre está nulo', (done) => {
+        Raza.create({id: 1000,
+          imagen: 'https://empresas.blogthinkbig.com/wp-content/uploads/2019/11/Imagen3-245003649.jpg?w=800',
+          altura: '12 - 16',
+          peso: '13 - 15',
+          anosvida: 12,
+          temperamentos: ['Aloof', 'Clownish', 'Dignified', 'Independent', 'Happy']})
+          .then(() => done('no crea el registro'))
+          .catch(() => done());
+      });
+    });
+    describe('altura', () => {
+      it('no crea el elemento si altura está nula', (done) => {
+        Raza.create({id: 1000,
+          imagen: 'https://empresas.blogthinkbig.com/wp-content/uploads/2019/11/Imagen3-245003649.jpg?w=800',
+          nombre: 'Pug',
+          peso: '13 - 15',
+          anosvida: 12,
+          temperamentos: ['Aloof', 'Clownish', 'Dignified', 'Independent', 'Happy']})
+          .then(() => done('no crea el registro'))
+          .catch(() => done());
+      });
+    });
+    describe('peso', () => {
+      it('no crea el elemento si peso está nulo', (done) => {
+        Raza.create({id: 1000,
+          imagen: 'https://empresas.blogthinkbig.com/wp-content/uploads/2019/11/Imagen3-245003649.jpg?w=800',
+          nombre: 'Pug',
+          altura: '13 - 15',
+          anosvida: 12,
+          temperamentos: ['Aloof', 'Clownish', 'Dignified', 'Independent', 'Happy']})
+          .then(() => done('no crea el registro'))
+          .catch(() => done());
+      });
+    });
+    describe('años de vida', () => {
+      it('crea el elemento si años de vida está nulo', (done) => {
+        Raza.create({id: 1000,
+          imagen: 'https://empresas.blogthinkbig.com/wp-content/uploads/2019/11/Imagen3-245003649.jpg?w=800',
+          nombre: 'Pug',
+          altura: '12 - 16',
+          peso: '13 - 15',
+          temperamentos: ['Aloof', 'Clownish', 'Dignified', 'Independent', 'Happy']})
+          .then(() => done())
+          .catch(() => done('no crea el registro'));
       });
     });
   });
--- api/tests/routes/dog.spec.js
+++ api/tests/routes/dog.spec.js
@@ -2,23 +2,45 @@
 const { expect } = require('chai');
 const session = require('supertest-session');
 const app = require('../../src/app.js');
-const { Dog, conn } = require('../../src/db.js');
+const { Raza, conn } = require('../../src/db.js');
 
 const agent = session(app);
 const dog = {
-  name: 'Pug',
+  id: 1000,
+  imagen: 'https://empresas.blogthinkbig.com/wp-content/uploads/2019/11/Imagen3-245003649.jpg?w=800',
+  nombre: 'Pug',
+  altura: '12 - 16',
+  peso: '13 - 15',
+  anosvida: '12',
+  temperamentos: ['Aloof', 'Clownish', 'Dignified', 'Independent', 'Happy']
 };
 
-describe('Videogame routes', () => {
+describe('Dog routes', () => {
   before(() => conn.authenticate()
-  .catch((err) => {
-    console.error('Unable to connect to the database:', err);
-  }));
-  beforeEach(() => Dog.sync({ force: true })
-    .then(() => Dog.create(dog)));
+    .catch((err) => {
+      console.error('Unable to connect to the database:', err);
+    }));
+  beforeEach(() => Raza.sync({ force: true })
+    .then(() => Raza.create(dog)));
   describe('GET /dogs', () => {
-    it('should get 200', () =>
+    it('deberia recibir 200', () =>
       agent.get('/dogs').expect(200)
+    );
+    it('deberia recibir 172 elementos', () =>
+      agent.get('/dogs').expect(function (res) {
+        expect(res.body).to.have.length(173)
+      })
+    );
+    it('deberia recibir 404', () =>
+      agent.get('/dogs?name=Holi').expect(404)
+    );
+    it('deberia recibir 200 con el elemento creado', () =>
+      agent.get('/dogs?name=PUG').expect(200)
+    );
+    it('deberia recibir el elemento creado', () =>
+      agent.get('/dogs?name=PUG').expect(function (res) {
+        expect(res.body).to.have.length(2)
+      })
     );
   });
 });
